582774ecded30aa0f34c6dc16e6847532a78a499
Chandan Kumar Sahoo <83700644+Chandan2026@users.noreply.github.com>
2021-08-07 15:04:21 +0530

    Add files via upload

---
+++ MLclustring.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 12:17:21 2021
+
+@author: chandan Kumar Sahoo
+"""
+
+import pandas as pd
+dataset=pd.read_csv("Mall_Customers.csv")
+
+X=dataset.iloc[:,2:5].values
+from sklearn.cluster import KMeans
+k_means=KMeans(n_clusters=5,init='k-means++',random_state=0)
+k_means.fit(X)
+print(k_means.labels_)
+wcss=[]
+for k in range(1,11):
+    km=KMeans(n_clusters=k,init='k-means++')
+    km.fit(X)
+    wcss_i=km.inertia_
+    wcss.append(wcss_i)
+    print(k,wcss_i)
+    
+import matplotlib.pyplot as plt
+plt.plot(range(1,11),wcss)
+plt.title("Elbow Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.show()
